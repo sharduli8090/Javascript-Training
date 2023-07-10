c3a2fcc5be16e92689e63a55570d12f47941f273
Sharduli Pandey <shardulipandey8090@gmail.com>
2023-07-10 17:24:39 +0530

    session 2 - 7th july

---
+++ Session Extra/index.js
@@ -0,0 +1,27 @@
+// var , let , const
+
+// function func_name(){  
+//         const x = 20; 
+//         console.log(x);
+//         x=22;
+    
+//     console.log(x);
+
+// } 
+
+// func_name();
+
+
+// var let const
+
+let x = 10;
+
+console.log(x);
+
+if(true){
+    const x = 20;
+    x=200;
+    console.log(x);
+}
+
+console.log(x);
